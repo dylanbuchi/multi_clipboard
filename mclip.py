3e1dc4878714357dea33d2d453ad65674faf400e
Dylan Buchi <devybuchi@gmail.com>
2020-07-25 07:49:54 -0300

    added bat file to run program

--- mclip.py
+++ mclip.py
@@ -1,1 +1,9 @@
-# mclip.py - A multi-clipboard program.
+#! python3
+
+TEXT = {
+    'agree': 'Yes, I agree. That sounds fine to me.',
+    'busy': 'Sorry, can we do this later this week or next week?',
+    'upsell': 'Would you consider making this a monthly donation?'
+}
+
+print("dfd")
