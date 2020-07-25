9966cfa171102de4f755c6c7feda3d8de69c9849
Dylan Buchi <devybuchi@gmail.com>
2020-07-25 09:16:31 -0300

    finish script

--- mclip.py
+++ mclip.py
@@ -1,9 +1,29 @@
-#! python3
+import sys
+import pyperclip
 
-TEXT = {
-    'agree': 'Yes, I agree. That sounds fine to me.',
-    'busy': 'Sorry, can we do this later this week or next week?',
-    'upsell': 'Would you consider making this a monthly donation?'
+#constant global variable dictionary LINK
+LINKS = {
+    'python': 'https://www.python.org/',
+    'github': 'https://github.com/',
+    'java': 'https://docs.oracle.com/en/java/',
+    'youtube': 'https://www.youtube.com/',
+    'reddit': 'https://www.reddit.com/',
 }
 
-print("dfd")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python mclip.py [keyphrase] - copy link")
+
+        # a default value copied
+        pyperclip.copy("Hellooo")
+        sys.exit()
+
+    # first command line arg is the keyphrase
+    keyphrase = sys.argv[1]
+
+    if keyphrase in LINKS:
+        #copy the value of the key into user copy-paste clip
+        pyperclip.copy(LINKS[keyphrase])
+        print(f"link for {keyphrase} website copied to clipboard.")
+else:
+    print(f"There is no link for {keyphrase}")
